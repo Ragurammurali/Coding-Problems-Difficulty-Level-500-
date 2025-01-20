8f4edd26e4bb54b48a033ce9780719b76b6350e3
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 09:47:59 +0530

    Code for the Problem Water Requirement
    This is the Code for the Problem: Water Requirement

---
+++ Water Requirement.py
@@ -0,0 +1,9 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/WATERREQ 
+- Difficulty: 351
+
+Code:
+T=int(input())
+for i in range(T):
+    N=int(input())
+    water=N*2
+    print(water)
