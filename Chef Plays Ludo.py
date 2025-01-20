7bc17903ce764f22b214497b830e9410e2341432
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 07:46:11 +0530

    Code for the Problem Chef Plays Ludo
    This is the Code for the Problem Chef Plays Ludo

---
+++ Chef Plays Ludo.py
@@ -0,0 +1,11 @@
+- link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUDO 
+- Difficulty: 260
+
+Code:
+T=int(input())
+for i in range(T):
+    X=int(input())
+    if(X==6):
+        print("YES")
+    else:
+        print("NO")
