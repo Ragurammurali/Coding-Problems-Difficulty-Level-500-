b601a4c6b91f575435a3b56b24db02ca6d9533aa
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 08:28:25 +0530

    Code for the Problem Counting Words
    This is the Code for the Problem: Counting Words

---
+++ Counting Words.py
@@ -0,0 +1,9 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CNTWRD 
+- Difficulty: 296
+
+Code:
+T=int(input())
+for i in range(T):
+    N,M=map(int,input().split())
+    total=N*M 
+    print(total)
