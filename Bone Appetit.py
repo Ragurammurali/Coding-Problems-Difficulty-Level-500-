2f76bff57f10969fb46eaa095c6895bcb304482e
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 08:10:26 +0530

    Code for the Problem Bone Appetit
    This is the Code for the Problem: Bone Appetit

---
+++ Bone Appetit.py
@@ -0,0 +1,8 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BNE_APT 
+- Difficulty: 280 
+
+Code:
+N,M=map(int,input().split())
+X,Y=map(int,input().split())
+total=N*X+M*Y
+print(total)
