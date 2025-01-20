2a1cd015bdd18e9e2f5978c34d761cf8ddf545ed
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 07:44:31 +0530

    Code for the Problem Biryani classes
    This is the Code for the Problem Biryani classes

---
+++ Biryani classes.py
@@ -0,0 +1,9 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BIRYANI
+- Difficulty: 257 
+
+Code:
+T=int(input())
+for i in range(T):
+    X,Y=map(int,input().split())
+    total=X*Y 
+    print(total)
