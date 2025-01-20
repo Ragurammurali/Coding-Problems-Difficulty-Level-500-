a9658177949afc36307e2459bc3de63d2b9ce407
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 08:17:09 +0530

    Code for the Problem 2000
    This is the Code for the Problem: 2000

---
+++ 2000.py
@@ -0,0 +1,10 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/RIP2000 
+- Difficulty: 284
+
+Code:
+N=int(input())
+value=2000
+total_amount=2000*N 
+value2=500
+no_of_five_hundread_rupees_notes_needed=(total_amount/value2)
+print(int(no_of_five_hundread_rupees_notes_needed))
