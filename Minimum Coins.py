f0a79ecfdacbeeecad3b005449d9e93282acdb06
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 10:02:36 +0530

    Code for the Problem Minimum Coins
    This is the Code for the Problem: Minimum Coins

---
+++ Minimum Coins.py
@@ -0,0 +1,9 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/MINCOINSREQ 
+- Difficulty: 390
+
+Code:
+T=int(input())
+for i in range(T):
+    X=int(input())
+    A=X%10
+    print(A)
