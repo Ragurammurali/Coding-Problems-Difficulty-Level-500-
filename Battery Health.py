6930c842bb194370f6f3ee34b94960fd947e52c4
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 08:29:50 +0530

    Code for the Problem Battery Health
    This is the Code for the Problem: Battery Health

---
+++ Battery Health.py
@@ -0,0 +1,11 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/BTRYHLTH 
+- Difficulty: 296 
+
+Code:
+T=int(input())
+for i in range(T):
+    X=int(input())
+    if(X>=80):
+        print("YES")
+    elif(X<80):
+        print("NO")
