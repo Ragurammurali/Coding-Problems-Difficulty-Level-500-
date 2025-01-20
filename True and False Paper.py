cfae13c5bf4de9c777d2dd050b2894ce847c2d84
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 10:08:53 +0530

    Code for the Problem True and False Paper
    This is the Code for the Problem: True and False Paper

---
+++ True and False Paper.py
@@ -0,0 +1,9 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TFPAPER 
+- Difficulty: 398
+
+Code:
+T=int(input())
+for i in range(T):
+    N,K=map(int,input().split())
+    B=N-K
+    print(B)
