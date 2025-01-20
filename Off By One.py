f686838011df6992f6f18ab57c4348b19f79cd4b
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 07:55:40 +0530

    Code for the Problem Off By One
    This is the Code for the Problem: Off By One

---
+++ Off By One.py
@@ -0,0 +1,7 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OFFBY1 
+- Difficulty: 271 
+
+Code:
+A,B=map(int,input().split())
+sum=(A+B)*10 
+print(sum+1)
