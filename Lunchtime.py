698d6be9ff753266a6f175493caf6609ebbac555
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 09:49:09 +0530

    Code for the Problem Lunchtime
    This is the Code for the Problem: Lunchtime

---
+++ Lunchtime.py
@@ -0,0 +1,11 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LTIME 
+- Difficulty: 352 
+
+Code:
+T=int(input())
+for i in range(T):
+    X=int(input())
+    if(X>=1 and X<=4):
+        print("YES")
+    else:
+        print("NO")
