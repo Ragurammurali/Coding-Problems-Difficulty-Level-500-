7956f0336ede846c902972aed3393eec00bb4ab0
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 09:28:10 +0530

    Code for the Problem October Marathon
    This is the Code for the Problem: October Marathon

---
+++ October Marathon.py
@@ -0,0 +1,11 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/OCTATHON 
+- Difficulty: 319
+
+Code:
+X=int(input())
+if(X<3):
+    print("GOLD")
+elif(X>=3 and X<6):
+    print("SILVER")
+elif(X>=6):
+    print("BRONZE")
