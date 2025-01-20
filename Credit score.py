00aeb903169601eb4808a1fdba2f5e514980997e
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 10:50:28 +0530

    Code for the Problem Credit score
    This is the Code for the Problem: Credit score

---
+++ Credit score.py
@@ -0,0 +1,9 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CREDSCORE
+- Difficulty: 459
+
+Code:
+X=int(input())
+if(X>=750):
+    print("YES")
+elif(X<750):
+    print("NO")
