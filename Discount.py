b6bc51e997d6c799b330b1aa6891176255b7a4b9
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 10:11:16 +0530

    Code for the Problem Discount
    This is the Code for the Problem: Discount

---
+++ Discount.py
@@ -0,0 +1,10 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DISCNT 
+- Difficulty: 401
+
+Code:
+T=int(input())
+for i in range(T):
+    X=int(input())
+    price=100
+    revised_price=price-X
+    print(int(revised_price))
