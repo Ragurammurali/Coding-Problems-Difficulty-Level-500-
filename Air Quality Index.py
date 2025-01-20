2d82a22cd2305260e23716b10bd160312dfba570
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 09:41:04 +0530

    Code for the Problem Air Quality Index
    This is the Code for the Problem: Air Quality Index

---
+++ Air Quality Index.py
@@ -0,0 +1,11 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AIRINDEX 
+- Difficulty: 347
+
+Code:
+X=int(input())
+if(X<100):
+    print("YES")
+elif(X>100):
+    print("NO")
+elif(X==100):
+    print("NO")
