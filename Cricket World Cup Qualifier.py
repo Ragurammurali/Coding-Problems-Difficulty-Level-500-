127efe05f00e310b21eaee3e70ad9e72a8132071
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 07:24:54 +0530

    Update and rename Cricket World Cup Qualifier to Cricket World Cup Qualifier.py

--- Cricket World Cup Qualifier.py
+++ Cricket World Cup Qualifier.py
@@ -1,2 +1,9 @@
 - link to The Question: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWC23QUALIF
 - Difficulty Rating: 203
+
+Code:
+X=int(input())
+if(X>=12):
+    print("Yes")
+else:
+    print("No")
