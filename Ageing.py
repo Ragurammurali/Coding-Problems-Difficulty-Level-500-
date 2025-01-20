502a259c0fb08e6c5e20140e701850e10bfd41de
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 08:32:24 +0530

    Code for the Problem Ageing
    This is the Code for the Problem: Ageing

---
+++ Ageing.py
@@ -0,0 +1,9 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/AGEING 
+- Difficulty: 299
+
+Code:
+T=int(input())
+for i in range(T):
+    X=int(input())
+    Y=X-10
+    print(Y)
