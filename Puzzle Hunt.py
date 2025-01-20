7caf408b2dda8dc11ab3162ea29becd495b50928
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 08:08:50 +0530

    Code for the Problem Puzzle Hunt
    This is the Code for the Problem: Puzzle Hunt

---
+++ Puzzle Hunt.py
@@ -0,0 +1,9 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PUZHUNT 
+- Difficulty: 279
+
+Code:
+N=int(input())
+if(N>=6 and N<=8):
+    print("Yes")
+else:
+    print("No")
