882cf86b0338282dda905ad335df4416ed8648ee
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 07:34:22 +0530

    Code for the problem Clear Day
    Code for the Question: Clear Day
    Link is attached with the Code for the Question

---
+++ Clear Day.py
@@ -0,0 +1,8 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CLEARDAY 
+- Difficulty: 233 
+
+Code:
+X,Y=map(int,input().split())
+total=7
+clear_days=total-(X+Y)
+print(clear_days)
