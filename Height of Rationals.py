22332afb6998d52dea59f5aeb5744e2ee9f15d29
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 10:13:54 +0530

    Code for the Problem Height of Rationals
    This is the Code for the Problem: Height of Rationals

---
+++ Height of Rationals.py
@@ -0,0 +1,7 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/HEIGHTRATION
+- Difficulty: 405
+
+Code:
+a,b=map(int,input().split())
+c=max(a,b)
+print(c)
