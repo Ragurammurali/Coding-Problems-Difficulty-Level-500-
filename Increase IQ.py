504296bf16d4f018d02516396d10e3c83e41e740
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 11:01:25 +0530

    Code for the Problem Increase IQ
    This is the Code for the Problem: Increase IQ

---
+++ Increase IQ.py
@@ -0,0 +1,13 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/INCRIQ
+- Difficulty: 478
+
+Code:
+X=int(input())
+MusicIQ=7
+ChefNewIQ=X+MusicIQ
+if(ChefNewIQ>170):
+    print("Yes")
+elif(ChefNewIQ<170):
+    print("No")
+elif(ChefNewIQ==170):
+    print("No")
