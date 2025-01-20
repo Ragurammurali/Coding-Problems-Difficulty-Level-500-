2d436a848cb7507210771b71c8bde986d2a9e021
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 07:48:15 +0530

    Code for the Problem How many unattempted problems
    This is the code for the problem: How many unattempted problems

---
+++ How many unattempted problems.py
@@ -0,0 +1,7 @@
+- link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACLIST 
+- Difficulty: 264 
+
+Code:
+X,Y=map(int,input().split())
+un_attempted_problems=X-Y
+print(un_attempted_problems)
