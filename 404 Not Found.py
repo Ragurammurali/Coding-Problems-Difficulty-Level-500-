56a553013519c55bfc794ceaedccf9bf6e48b05b
Ragurammurali <73242015+Ragurammurali@users.noreply.github.com>
2025-01-20 07:52:13 +0530

    Code for the Problem 404 Not Found
    This is the Code for the Problem: 404 Not Found

---
+++ 404 Not Found.py
@@ -0,0 +1,9 @@
+- Link: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404 
+- Difficulty: 267 
+
+Code:
+X=int(input())
+if(X==404):
+    print("NOT FOUND")
+else:
+    print("FOUND")
